a3c1557f2431d96bfb3d103e8a68d36a79f4b550
Saksham-Batra <140792075+Saksham-Batra@users.noreply.github.com>
2024-04-19 18:47:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 function convertToRoman(num) {
-  	const symbols = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
-let roman = '';
-	for(let i = 0; i<symbols.length; i++){
-while(num>=symbols[i][1]){
-	roman += symbols[i][0];
-	num -= symbols[i][1];
-}
-		if(i%2==0 && i<symbols.length-2){
-const next = symbols[i+2][0];
-			const nextVal = symbols[i+2][1];
-			if(num>=symbols[i][1] -nextVal){
-				roman+=next+symbols[i][0];
-				num-=symbols[i][1]-nextVal;
-			}
-		}
-	}
-return roman;
+    const symbols = [
+        ['M', 1000],
+        ['D', 500],
+        ['C', 100],
+        ['L', 50],
+        ['X', 10],
+        ['V', 5],
+        ['I', 1]
+    ];
+
+    let romanNumeral = '';
+
+    for (let i = 0; i < symbols.length; i++) {
+        while (num >= symbols[i][1]) {
+            romanNumeral += symbols[i][0];
+            num -= symbols[i][1];
+        }
+
+        // Check for subtractive notation
+        if (i % 2 === 0 && i < symbols.length - 2) {
+            const nextSymbol = symbols[i + 2][0];
+            const nextValue = symbols[i + 2][1];
+
+            if (num >= symbols[i][1] - nextValue) {
+                romanNumeral += nextSymbol + symbols[i][0];
+                num -= symbols[i][1] - nextValue;
+            }
+        }
+    }
+
+    return romanNumeral;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
